61914d2cd857784854911691c5fa735b84dcbb9f
praveen-jalaja <58303029+praveen-jalaja@users.noreply.github.com>
2021-05-29 18:09:15 +0530

    update app.py

--- app.py
+++ app.py
@@ -73,6 +73,9 @@ st.write("""
 trans = SpokenToWritten.spk2wrt()
 
 #========================== text Uploader ===================================
+st.text("Sample")
+st.text("Copy Paste the below text and translate")
+st.text("My life is Triple B . European authorities fined Google a record sixty five thousand dollars on Wednesday for abusing its power in the mobile phone market and ordered the company to alter its practices . Furthermore , My T - Shirt size is double X in 2019 and it costs six dollars . My weight is fifty kilograms.")
 
 user_input = st.text_area("Paragraph here", "")
 
